4dad72237c573d1f064bca70c3c913cf7ecce6d4
Hyunzzang <phc023@gmail.com>
2021-01-16 10:19:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# newsfeed
+ news feed api
---
+++ src/test/java/com/hyunzzang/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunzzang.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
